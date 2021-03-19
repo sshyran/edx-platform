ff538ae38872dfeaa33aee1f8095fe841da38bb3
Thomas Tracy <contact@tjtracy.com>
2021-03-19 14:20:19 -0400

    feat: Add command to generate credentials config
    Adds a command to create an API connection to credentials for testing
    program certificates on devstack. This command is not meant to be ran
    manually, and will be included in a provisioning type script that will
    be added later.

---
+++ openedx/core/djangoapps/credentials/management/commands/create_credentials_api_configuration.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from openedx.core.djangoapps.credentials.models import CredentialsApiConfig
+
+class Command(BaseCommand):
+"""
+Creates a api configuration between LMS <--> Credentials service.
+This command is meant to be used in combination with other commands to
+create a fully connected path to awarding program certificates in devstack.
+"""
+    def handle(self, *args, **kwargs):
+        try:
+            CredentialsApiConfig.objects.create(
+                enabled=True,
+                enable_learner_issuance=True,
+            )
+        except Exception as e:
+            raise CommandError(e)
